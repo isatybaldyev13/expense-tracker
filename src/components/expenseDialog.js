c5d982690268205f7e72b7be5be02f30212165f6
isatybaldyev13 <isatybaldyev@gmail.com>
2024-04-21 11:41:32 +0600

    ExpenseDialog compenent created

--- src/components/expenseDialog.js
+++ src/components/expenseDialog.js
@@ -1,4 +1,24 @@
-const ExpenseDialog = () => {
+import { Box, Button, Dialog, TextField, Typography } from "@mui/material";
+
+const ExpenseDialog = ({open, onClose}) => {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <Box
+        minWidth="400px"
+        p="1.5rem"
+        display="flex"
+        flexDirection="column"
+        gap={1}
+      >
+        <Typography variant="h6">Add Expense</Typography>
+        <TextField label="Location" variant="standard" />
+        <TextField label="Location Address" variant="standard" />
+        <TextField label="Items" variant="standard" />
+        <TextField label="Amount" variant="standard" />
+        <Button variant="contained">Submit</Button>
+      </Box>
+    </Dialog>
+  );
 };
 
 export default ExpenseDialog;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,16 +1,9 @@
-import {
-  Box,
-  Button,
-  Container,
-  Dialog,
-  IconButton,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Container, IconButton, Typography } from "@mui/material";
 import { useAuth } from "../hooks/useAuth";
 import Header from "../components/header";
 import { Add } from "@mui/icons-material";
 import { useState } from "react";
+import ExpenseDialog from "../components/expenseDialog";
 
 const Dashboard = () => {
   const [form, setForm] = useState(false);
@@ -29,22 +22,7 @@ const Dashboard = () => {
             <Add />
           </IconButton>
         </Box>
-        <Dialog open={form} onClose={() => setForm(false)}>
-          <Box
-            minWidth="400px"
-            p="1.5rem"
-            display="flex"
-            flexDirection="column"
-            gap={1}
-          >
-            <Typography variant="h6">Add Expense</Typography>
-            <TextField label="Location" variant="standard" />
-            <TextField label="Location Address" variant="standard" />
-            <TextField label="Items" variant="standard" />
-            <TextField label="Amount" variant="standard" />
-            <Button variant="contained">Submit</Button>
-          </Box>
-        </Dialog>
+        <ExpenseDialog open={form} onClose={() => setForm(false)} />
       </Container>
     </>
   );
